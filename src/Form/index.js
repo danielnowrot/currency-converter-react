7836f63bebc485b771160022676ed4f3c1c72d24
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-01-29 18:07:08 +0100

    added loading and error text info

--- src/Form/index.js
+++ src/Form/index.js
@@ -14,47 +14,71 @@ export const Form = ({ calculateResult, result, ratesData }) => {
 
     return (
         <Wrapper onSubmit={onFormSubmit}>
-            <p>
-                <Label>
-                    <LabelText>Wprowadź wartość*:</LabelText>
-                    <Input
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        name="currencyValue"
-                        type="number"
-                        min="0.01"
-                        step="any"
-                        placeholder="PLN"
-                        required />
-                </Label>
-            </p>
-            <p>
-                <Label>
-                    <LabelText>Wybierz walutę:</LabelText>
-                    <Select
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                        name="selectCurrency">
-                        {Object.keys(ratesData.rates).map((rates => (
-                            <option
-                                key={rates}
-                                value={rates}
-                            >
-                                {rates}
-                            </option>
-                        )))}
-                    </Select>
-                </Label>
-            </p>
-            <p>
-                <Button>POLICZ</Button>
-            </p>
-            <StyledResult>
-                <Result result={result} />
-            </StyledResult>
-            <InfoText>
-                * - pole wymagane
-            </InfoText>
+            {ratesData.status === "loading"
+                ? (
+                    <p>
+                        <Label>
+                            <LabelText>Ładujemy potrzebne dane 😁</LabelText>
+                        </Label>
+                    </p>
+                )
+                : (ratesData.status === "error")
+                    ? (
+                        <p>
+                            <Label>
+                                <LabelText>Przepraszamy, ale coś poszło nie tak😞
+                                    Sprawdź połączenie z internetem, albo spróbuj później.
+                                </LabelText>
+                            </Label>
+                        </p>
+                    )
+                    : (
+                        <>
+                            <p>
+                                <Label>
+                                    <LabelText>Wprowadź wartość*:</LabelText>
+                                    <Input
+                                        value={amount}
+                                        onChange={({ target }) => setAmount(target.value)}
+                                        name="currencyValue"
+                                        type="number"
+                                        min="0.01"
+                                        step="any"
+                                        placeholder="PLN"
+                                        required />
+                                </Label>
+                            </p>
+                            <p>
+                                <Label>
+                                    <LabelText>Wybierz walutę:</LabelText>
+                                    <Select
+                                        value={currency}
+                                        onChange={({ target }) => setCurrency(target.value)}
+                                        name="selectCurrency">
+                                        {Object.keys(ratesData.rates).map((rates => (
+                                            <option
+                                                key={rates}
+                                                value={rates}
+                                            >
+                                                {rates}
+                                            </option>
+                                        )))}
+                                    </Select>
+                                </Label>
+                            </p>
+                            <p>
+                                <Button>POLICZ</Button>
+                            </p>
+                            <StyledResult>
+                                <Result result={result} />
+                            </StyledResult>
+                            <InfoText>
+                                * - pole wymagane
+                            </InfoText>
+                        </>
+                    )
+            }
+
 
         </Wrapper>
     )
